a62ecc8962ed962909c15ebd0343d8780a5dada9
amjadalsh <117071119+amjadalsh@users.noreply.github.com>
2022-11-30 14:12:33 +0300

    Update and rename MR_Job_Movies.py to Movies_MR.py

--- Movies_MR.py
+++ Movies_MR.py
@@ -1,25 +1,26 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-class RatingsBreakdown_V2(MRJob):
+class RatingsBreakdown(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_ratings,
-                   reducer=self.reducer_count_ratings)
+                   reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.reducer_sorted_output)
         ]
 
     def mapper_get_ratings(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
         yield movieID,1
         
-        
-    def mapper_get_ratings_1(self, _, line):
-        (userID, movieID, rating, timestamp) = line.split('\t')
-        yield rating,1
-                
 
     def reducer_count_ratings(self, key, values):
-        yield key, sum(values)
+        yield str(sum(values)).zfill(5), key
+        
+        
+    def reducer_sorted_output(self, count, values):
+        for movie in movies:
+            yield movie, count
 
 if __name__ == '__main__':
-    RatingsBreakdown_V2.run()
+    RatingsBreakdown.run()
